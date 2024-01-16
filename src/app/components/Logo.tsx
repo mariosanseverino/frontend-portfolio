ffde340c3e1b4fe71a32d362efcad9da1feb8585
Mário Sanseverino <51727648+mariosanseverino@users.noreply.github.com>
2024-01-16 13:39:34 -0300

    Revert "Creating the initial wireframe for the page"

--- src/app/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Logo() {
-	return (
-		<p className='lg:order-1'>Logo</p>
-	);
-}
--- src/app/components/Slide3.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Slide3() {
-	return (
-		<div className='swiper-slide flex flex-col justify-centerder' id='slide-3'>
-		</div>
-	);
-}
