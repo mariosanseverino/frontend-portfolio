a5154ac831e8a506cebe361dab7727a5b13979bb
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-01-22 12:38:00 -0300

    feat: First version of the handleSwipe is working, but need small adjusments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ import Menu from './components/Menu';
 export default function Home() {
 	const { currentSlide, setCurrentSlide } = useBehaviorContext();
 	const totalSlides = 4;
+	let isScrolling = false;
+	let touchStart = 0;
+	let touchEnd = 0;
+	let isSwiping = false;
 
 	const handleWheel = useCallback(
 		(event: WheelEvent) => {
@@ -25,7 +29,6 @@ export default function Home() {
 		}, [totalSlides]
 	);
 	
-
 	const debouncedHandleWheel = useCallback(
 		(event: WheelEvent) => {
 			// Checks and prevents fast scrolling
@@ -41,11 +44,44 @@ export default function Home() {
 		}, [handleWheel]
 	);
 
-	let isScrolling = false;
+	function handleSwipe() {
+		const delta = touchEnd - touchStart;
+		const threshold = 50; // Minimum swipe distance
+			
+		setCurrentSlide((prevSlide) => {
+			if (delta < threshold) {
+				return Math.min(prevSlide + 1, totalSlides - 1);
+			} else if (delta !== 0 && delta > threshold) {
+				return Math.max(prevSlide - 1, 0);
+			}
+			return prevSlide;
+		});
+	}
+
+	const debouncedSwipe = useCallback(
+		() => {
+			if (!isSwiping) {
+				isSwiping = true;
+
+				handleSwipe();
+
+				setTimeout(() => {
+					isSwiping = false;
+				}, 1000);
+			}
+		}, [handleSwipe]
+	);
 
 	useEffect(() => {
 		// Add the mouse wheel scroll event listener when the component is mounted
 		document.addEventListener('wheel', debouncedHandleWheel);
+		document.addEventListener('touchstart', (event: TouchEvent) => {
+			touchStart = event.changedTouches[0].screenY;
+		});
+		document.addEventListener('touchend', (event: TouchEvent) => {
+			touchEnd = event.changedTouches[0].screenY;
+			debouncedSwipe();
+		});
 
 		// Remove the event listener when the component is unmounted
 		return () => {
