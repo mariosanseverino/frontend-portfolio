31f056fbf8a8bf2f02b9a0b88b84863b67273fc7
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-01-21 22:47:42 -0300

    fix: Removing unnecessary export of the font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Archivo, Archivo_Black } from 'next/font/google';
+import { Archivo } from 'next/font/google';
 import BehaviorProvider from './context/BehaviorContext';
 import NavBar from './components/NavBar';
 import type { Metadata } from 'next';
@@ -9,18 +9,11 @@ export const metadata: Metadata = {
 	title: 'Mario Sanseverino\'s Portfolio',
 };
 
-export const archivo = Archivo({
+const archivo = Archivo({
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 	display: 'swap',
 	subsets: ['latin'],
 	variable: '--archivo-font'
-});
-
-export const archivoBlack = Archivo_Black({
-	weight: ['400'],
-	display: 'swap',
-	subsets: ['latin'],
-	variable: '--archivoblack-font'
 });
 
 export default function RootLayout({
