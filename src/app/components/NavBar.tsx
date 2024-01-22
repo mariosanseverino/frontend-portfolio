30dfc115f16e34f0467a48d21631c7462a6e6a6a
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-01-22 19:07:53 -0300

    style: New padding test

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar() {
 	const { currentSlide, menu } = useBehaviorContext();
 
 	return (
-		<nav className={`h-16 z-10 w-full fixed top-0 text-lightblue px-4 flex flex-row justify-between items-center transition-colors duration-1000
+		<nav className={`h-16 z-10 w-full fixed top-0 px-8 text-lightblue flex flex-row justify-between items-center transition-colors duration-1000
 		lg:top-0 ${ currentSlide % 2 === 0 ? 'text-white' : 'text-lightblue' }`}>
 			<div className={ `flex gap-4 transition-opacity duration-1000 ${ menu ? 'opacity-100' : 'opacity-0' }` }>
 				<SocialMediaBtn type='linkedin' />
