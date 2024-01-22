a0733863a6e322a9a2fd66068e57c9eb64e9c4f4
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-01-21 21:45:02 -0300

    refactor: Updated my professional description in the about page

--- src/app/components/AboutPage.tsx
+++ src/app/components/AboutPage.tsx
@@ -4,38 +4,37 @@ import DowloadResumeBtn from './DownloadResumeBtn';
 
 export default function AboutPage() {
 	const techstack = ['HTML', 'CSS', 'JavaScript', 'React', 'Python', 'C-Sharp'];
-
+	
 	return (
 		<section className='swiper-slide bg-white text-black flex flex-col justify-between text-left' id='slide-2'>
-			{/* Content for slide 2 */}
 			<div className='flex flex-col gap-4'>
 				<div className='flex flex-col gap-4'>
 					<h1 className='uppercase'>About me</h1>
 					<p>
-						I&apos;m a versatile <strong>Front-end Developer</strong> who have worn different hats in my career,
-						such as a Graphic Designer and as a Digital Marketing Analyst, ultimately finding my passion in Web Development.
+					I&apos;m a <strong>Front-end Developer</strong> with long years of professional experience in different areas,
+					such as a Graphic Designer and as a Digital Marketing Analyst, ultimately finding my passion in Web Development.
 					</p>
 					<p>
-						<strong>Fluent in English and Portuguese</strong>, also being conversational in Italian, I bring quick learning,
-						strong interpersonal skills, and creativity to collaborative and fast-paced environments.
+						<strong>Fluent in English and Portuguese</strong>, also being conversational in Italian, I can highlight myself as a quick learner,
+					highly empathetic work mate and a very creative problem-solver.
 					</p>
 				</div>
 				<div className='flex flex-col gap-4'>
 					<h1 className='uppercase'>Stack</h1>
 					<div className='grid grid-cols-3 gap-y-4 justify-center'>
-						{techstack.map((tech, index) => (
-							<div key={index} className='flex flex-col items-center'>
+						{ techstack.map((tech, index) => (
+							<div key={ index } className='flex flex-col items-center'>
 								<img
-									src={`/icons/${tech.toLowerCase()}.png`}
-									alt={`${tech} icon`}
+									src={ `/icons/${ tech.toLowerCase() }.png` }
+									alt={ `${ tech } icon` }
 									className='w-11'
 								/>
-								<span className='font-bold'>{tech === 'C-Sharp' ? 'C#' : tech}</span>
+								<span className='font-bold'>{ tech === 'C-Sharp' ? 'C#' : tech }</span>
 							</div>
-						))}
+						)) }
 					</div>
 					<p>Everything about my education and all technologies I work with are detailed on my resume,
-						which you can download below. You can also find very precise information about me on my LinkedIn page.</p>
+						which you can download below. You can also find very precise information about my career on my LinkedIn page.</p>
 				</div>
 			</div>
 			<DowloadResumeBtn />
