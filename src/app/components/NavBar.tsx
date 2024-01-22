d981603a9f272467986d14e5c6b9d132cc2dbba6
Mario Sanseverino <marioaugusto.sanseverino@gmail.com>
2024-01-22 16:14:02 -0300

    fix: NavBar now positioned on the top right corner of the page

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -8,9 +8,9 @@ export default function NavBar() {
 	const { currentSlide, menu } = useBehaviorContext();
 
 	return (
-		<nav className={`h-16 z-10 w-full fixed bottom-0 text-lightblue px-[3rem] flex flex-row justify-between items-center transition-colors duration-1000
+		<nav className={`h-16 z-10 w-full fixed top-0 text-lightblue px-4 flex flex-row justify-between items-center transition-colors duration-1000
 		lg:top-0 ${ currentSlide % 2 === 0 ? 'text-white' : 'text-lightblue' }`}>
-			<div className={ `flex gap-4 ${ menu ? 'opacity-100' : 'opacity-0' }` }>
+			<div className={ `flex gap-4 transition-opacity duration-1000 ${ menu ? 'opacity-100' : 'opacity-0' }` }>
 				<SocialMediaBtn type='linkedin' />
 				<SocialMediaBtn type='github' />
 			</div>
